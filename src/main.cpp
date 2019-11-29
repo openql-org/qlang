e11b0f618851208d02d1f065afbd516763cb61f7
hirotakaster <hirotakaster@gmail.com>
2019-11-29 18:24:52 +0900

    llvm compiler options.

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,45 @@
 #include <llvm/IR/InlineAsm.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/InitLLVM.h>
+
 #include <string>
 
 #include "llvm_frontend.hpp"
+#include "option_parser.h"
 
 using namespace qlang;
 
+static void help(int exit_code = 1)
+{
+  fprintf(stderr, "qlang compiler\n\n");
+  fprintf(stderr, "usage: qlang [options] <target source code>\n");
+  fprintf(stderr, "options:\n");
+  fprintf(stderr, "  -O<n>                 optimizer leve\n");
+  exit(exit_code);
+}
+
+static void suggest_help() {
+  fprintf(stderr, "Try 'qlang --help' for more information.\n");
+  exit(1);
+}
+
 int main(int argc, char **argv) {
+  llvm::InitLLVM X(argc, argv);
+  int optimize = 0;
 
-  qlang::Frontend frontend(argv[1]);
+  option_parser_t parser;
+  parser.help(&suggest_help);
+  parser.option('h', "help", 0, [&](const char* s){help(0);});
+  parser.option('O', 0, 1, [&](const char* s){optimize = atoi(s);});
+  auto argv1 = parser.parse(argv);
+  std::vector<std::string> htif_args(argv1, (const char*const*)argv + argc);
+
+  if (!*argv1)
+    help();
+
+  qlang::Frontend frontend(htif_args[0]);
   frontend.compile();
-
   llvm::legacy::PassManager pm;
 
   // generate bitcode
