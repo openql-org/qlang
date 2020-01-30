c46e599c19f60ef2e70b064952244427ed405044
hirotakaster <hirotakaster@gmail.com>
2020-01-30 15:16:15 +0900

    add test_q.c test file.

---
+++ example/test_q.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int ret1 = 0;
+    int ret2 = 0;
+
+    // call qooh
+    asm volatile( "qooz.k qzero,qt1,qzero,1");
+    asm volatile( "qooz.k qzero,qt1,qzero,1");
+    asm volatile( "qooh.k qzero,qt1,qzero,1");
+    asm volatile( "qooh.k qzero,qt1,qzero,16");
+
+    // call qmeas
+    asm volatile( "qmeas.k  %0,qt1,qzero,0" :"=r"(ret1));
+    printf("%d\n", ret1);
+    asm volatile( "qmeas.k  %0,qt1,qzero,1" :"=r"(ret2));
+    printf("%d\n", ret2);
+    
+    return 0;
+}
